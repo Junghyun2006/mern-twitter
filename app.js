ed4407fdaa76de65b931926301daf766abe5949d
junghyun2006 <jung.hyun.park516@gmail.com>
2021-02-01 02:39:33 -0500

    twit test

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path')
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 //db and username are imported from config/key files which are then used below to connect
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -33,8 +27,16 @@ app.use("/api/users", users);
 app.use("/api/tweets", tweets); 
 //body parser is a middleware to parse the JSON sent to the front end (similar to Jbuilder?)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 //establishing which port to user
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
 
